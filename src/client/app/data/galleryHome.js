8fc5215258554f5e3524425e7f6798479a6c9598
daveyx <d.rehle@gmail.com>
2017-03-19 12:13:07 +0100

    added infos

--- src/client/app/data/galleryHome.js
+++ src/client/app/data/galleryHome.js
@@ -4,42 +4,52 @@ const images = [
   {
     original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_01.jpg',
     thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_01.jpg',
+    description: 'We took only one luggage, as all lowcost carriers want the customer to pay extra for each luggage.'
   },
   {
     original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_02.jpg',
     thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_02.jpg',
-  },
-  {
-    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_10.jpg',
-    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_10.jpg',
-  },
-  {
-    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_20.jpg',
-    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_20.jpg',
-  },
-  {
-    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_30.jpg',
-    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_30.jpg',
-  },
-  {
-    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_40.jpg',
-    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_40.jpg',
-  },
-  {
-    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_50.jpg',
-    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_50.jpg',
+    description: 'As you can see, only one luggage was enough for us two... Any comments?'
   },
   {
     original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_70.jpg',
     thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_70.jpg',
+    description: 'Near Phuket airport we had a bit time for a ice...'
   },
   {
     original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_80.jpg',
     thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_80.jpg',
+    description: ''
   },
   {
     original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_90.jpg',
     thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_90.jpg',
+    description: 'Bye bye Phuket, will miss you for sure, see ya soon...'
+  },
+  {
+    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_10.jpg',
+    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_10.jpg',
+    description: 'Beautiful Mai Khao beach of Phuket'
+  },
+  {
+    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_20.jpg',
+    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_20.jpg',
+    description: "What's wrong with this pic??? YES! Farang is sleeping, while thai girl take a pic..."
+  },
+  {
+    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_30.jpg',
+    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_30.jpg',
+    description: 'Welcome to Chiang Rai!'
+  },
+  {
+    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_40.jpg',
+    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_40.jpg',
+    description: ''
+  },
+  {
+    original: 'http://daveyx.ga/chiangrai-trip/img/day1/day1_1_50.jpg',
+    thumbnail: 'http://daveyx.ga/chiangrai-trip/img/day1/thumbs/day1_1_50.jpg',
+    description: 'This is a kind of steamed egg in banana leafs, very delicious! We had a lot of other food at a local market, but we did not take enough pictures, but the emotions are saved in our hearts for ever.'
   }
 ];
 
