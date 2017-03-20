0e8e986ee0ef58c4a4785e603b255714ed26fc34
daveyx <d.rehle@gmail.com>
2017-03-20 15:01:13 +0100

    first part of separated data for day pages

--- src/client/app/components/day.jsx
+++ src/client/app/components/day.jsx
@@ -10,11 +10,30 @@ import EmptyDay from './emptyDay.jsx';
 //   imgName: BASENAME + "img/watrongkhun.jpg"
 // }
 
-const childStates = {};
+const childStates = {data: {}};
 
 export default class Day extends BasePage {
   constructor(props) {
     super('', childStates);
+  }
+
+  componentDidMountChild() {
+    var data = require('../data/day1.json');
+    this.setState({
+      data: data
+    });
+    console.log(this.state.data);
+    // read the day-data json:
+    // if (this.state.gallery.length == 0) {
+    //   axios.get("http://www.daveyx.ga/data/day1.json")
+    //     .then(response => {
+    //     this.setState({
+    //       gallery: response.data.data
+    //     });
+    //   }).catch(function (error) {
+    //     console.log("error axios-get1: " + error);
+    //   });
+    // }
   }
 
   handleResizeChild(e = null) {
@@ -23,14 +42,14 @@ export default class Day extends BasePage {
   renderContent() {
     return (
         <main ref="content">
-          <Grid fluid ref="day" className="content" style={this.state.contentStyle}>
+          <Grid fluid ref="day" className={'day ' + this.props.location.pathname} style={this.state.contentStyle}>
             {this.props.location.pathname !== "day1" ? <EmptyDay pathname={this.props.location.pathname} />
             :
               <Row>
                 <Col xs={12}>
-                  <h1 className="text-center home-h1">
-                   peter
-                 </h1>
+                    <h1 className="text-center home-h1">
+                      Day 1 of our trip - Exploring Chiang Rai
+                    </h1>
                </Col>
              </Row>
             }
