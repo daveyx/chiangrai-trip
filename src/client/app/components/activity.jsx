4619c83c55668cd9f1a6aa84f75199f0b5924e15
daveyx <d.rehle@gmail.com>
2017-03-23 16:11:59 +0100

    more data

--- src/client/app/components/activity.jsx
+++ src/client/app/components/activity.jsx
@@ -29,7 +29,7 @@ export default class Activity extends Component {
     }
     let content =
                 <Row>
-                  <Col xs={12} md={6} xsPush={pullPushXs} mdPush={pullPushMd}>
+                  <Col xs={12} md={6} mdPush={pullPushMd}>
                     {this.props.data.activity.position.coordinates.lng > 0 && this.props.data.activity.position.coordinates.lat > 0 ?
                       <div>
                         <h3>{this.props.data.activity.position.header}</h3>
@@ -40,7 +40,7 @@ export default class Activity extends Component {
                     : <p>Google Maps not available</p>
                     }
                   </Col>
-                  <Col xs={12} md={6} xsPull={pullPushXs} mdPull={pullPushMd}>
+                  <Col xs={12} md={6} mdPull={pullPushMd}>
                     <h3>{this.props.data.activity.activityHeader2}</h3>
                     {this.props.data.activity.activityBody.map(function(data, index){
                       return <p key={index}>{data}</p>;
@@ -58,7 +58,7 @@ export default class Activity extends Component {
         {content}
         <Row>
           <Col xs={12}>
-            <h2 className="text-center">{this.props.data.activity.galleryHeader}</h2>
+            <h3 className="text-center">{this.props.data.activity.galleryHeader}</h3>
             <p>(click on the bottom right for full screen mode)<br />(click the 'play' button on the bottom left for a image show)</p>
             <Gallery images={this.props.data.activity.images} />
           </Col>
