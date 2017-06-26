a7a5a6103bd00852892d2dcf9db3acf90855284b
daveyx <d.rehle@gmail.com>
2017-06-26 05:22:19 +0200

    added submit handler

--- dev/components/user/loginForm.js
+++ dev/components/user/loginForm.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import { Row, Col, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import ReactDOM from'react-dom';
+import { Row, Col, Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 export default class LoginForm extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const email = ReactDOM.findDOMNode(this.refs.emailField).value;
+    const password = ReactDOM.findDOMNode(this.refs.passwordField).value;
+    console.log(email, password);
   }
 
   render() {
     return (
         <Row>
           <Col xs={12}>
-            <form>
+            <Form id="login-form" onSubmit={(e) => this.handleSubmit(e)}>
               <FormGroup controlId="formControlsEmail">
                 <ControlLabel>Email address</ControlLabel>
                 <FormControl
                   type="email"
                   placeholder="Enter email"
+                  ref="emailField"
                 />
               </FormGroup>
               <FormGroup controlId="formControlsPassword">
@@ -23,9 +33,13 @@ export default class LoginForm extends Component {
                 <FormControl
                   type="password"
                   placeholder="Enter password"
+                  ref="passwordField"
                 />
               </FormGroup>
-            </form>
+                <Button ref="sendButton" type="submit">
+                  Login
+                </Button>
+            </Form>
           </Col>
         </Row>
       );
