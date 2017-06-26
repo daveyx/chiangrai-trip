29fc1312abfc87699704ce06c06a73d139276b70
daveyx <d.rehle@gmail.com>
2017-06-26 05:22:49 +0200

    code format

--- dev/components/user/loginForm.js
+++ dev/components/user/loginForm.js
@@ -17,31 +17,31 @@ export default class LoginForm extends Component {
 
   render() {
     return (
-        <Row>
-          <Col xs={12}>
-            <Form id="login-form" onSubmit={(e) => this.handleSubmit(e)}>
-              <FormGroup controlId="formControlsEmail">
-                <ControlLabel>Email address</ControlLabel>
-                <FormControl
-                  type="email"
-                  placeholder="Enter email"
-                  ref="emailField"
-                />
-              </FormGroup>
-              <FormGroup controlId="formControlsPassword">
-                <ControlLabel>Password</ControlLabel>
-                <FormControl
-                  type="password"
-                  placeholder="Enter password"
-                  ref="passwordField"
-                />
-              </FormGroup>
-                <Button ref="sendButton" type="submit">
-                  Login
-                </Button>
-            </Form>
-          </Col>
-        </Row>
-      );
-    }
+      <Row>
+        <Col xs={12}>
+          <Form id="login-form" onSubmit={(e) => this.handleSubmit(e)}>
+            <FormGroup controlId="formControlsEmail">
+              <ControlLabel>Email address</ControlLabel>
+              <FormControl
+                type="email"
+                placeholder="Enter email"
+                ref="emailField"
+              />
+            </FormGroup>
+            <FormGroup controlId="formControlsPassword">
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                type="password"
+                placeholder="Enter password"
+                ref="passwordField"
+              />
+            </FormGroup>
+              <Button ref="sendButton" type="submit">
+                Login
+              </Button>
+          </Form>
+        </Col>
+      </Row>
+    );
   }
+}
